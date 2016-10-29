ac00ff70f6a308cdad4113439977cd010f053c2d
Bartholomew Krystman <bkrystman@protonmail.ch>
2016-10-29 16:51:14 -0600

    firebase pod added with GoogleService-Info.plist

---
+++ Pods/Target Support Files/Pods-meal_finder/Pods-meal_finder-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_meal_finderVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_meal_finderVersionString[];
+
