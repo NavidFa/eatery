20fb8d8caf16ca3c86c6ed982353fbdddbcb5918
Bartholomew Krystman <bkrystman@protonmail.ch>
2016-10-31 11:28:28 -0600

    alamofire is connected to an api and getting correct data

--- meal_finder/AlamoGet.swift
+++ meal_finder/AlamoGet.swift
@@ -13,8 +13,14 @@ func Alamo() {
     let n = "7"
     
     Alamofire.request(URL(number: n)).responseJSON { response in
+        print(response)
         let result = response.result // whenever we get a response in a JSON file we want to capture that data in raw format and pass that in as a directory
+        
         if let dict = result.value as? Dictionary<String, AnyObject> {
+            print("\(dict)")
+        }
+        
+        /*if let dict = result.value as? Dictionary<String, AnyObject> {
             print("\(dict)")
             if let name = dict["name"] as? String {
                 print(name)
@@ -27,6 +33,6 @@ func Alamo() {
                 print(arr)
             }
             
-        }
+        }*/
     }
 }
